9413a757407a94b66a1bd1d0168b0d52b8d130ef
EmiliyaAndamova <EmiliyaAndamova@WPIPCM>
2022-05-25 18:21:15 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _220524_Classwork
+{
+    class Program
+    {
+        public static object Username { get; private set; }
+
+        public static void Main(string[] args)
+        {
+            Student Ali = new Student          
+            {
+                Name = "Ali",
+                Surname = "Aliyev",
+                Username = "Aali",
+                Password = "a111",
+                Grade 
+            };
+            Student Mammed = new Student
+            {
+                Name = "Mammed",
+                Surname = "Mammedov",
+                Username = "Mmammed",
+                Password = "m111"
+            };
+            Student Qurban = new Student
+            {
+                Name = "Qurban",
+                Surname = "Qurbanov",
+                Username = "Qqurban",
+                Password = "q111"
+            };
+
+            Student[] students = {Ali, Mammed, Qurban};
+
+            Console.WriteLine("Please inter Username and Password: ");
+            string studentusername = Console.ReadLine();
+            string studentpassword = Console.ReadLine();
+
+            bool flag = false; 
+            foreach (Student student in students)
+            {
+                if (student.Username == studentusername)
+                {
+                    flag = true;
+                    student.GetData();
+                }
+                if (!flag) Console.WriteLine(Grade);    
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _220524_Classwork
+{
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public string Username;
+        public string Password;
+
+        public void GetData()
+        {
+            Console.WriteLine($"username = {Username}, password = {Password}");
+        }
+    }
+}
